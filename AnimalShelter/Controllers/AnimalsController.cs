90edaa1aa49fcdc2d8fd01bcc0497ac285a79141
Christen Weston <paleeserecycle@gmail.com>
2022-03-01 20:31:37 -0800

    Basic animal shelter created!

---
+++ AnimalShelter/Controllers/AnimalsController.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Mvc;
+using AnimalShelter.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AnimalShelter.Controllers
+{
+  public class AnimalsController : Controller
+  {
+    private readonly AnimalShelterContext _db;
+
+    public AnimalsController(AnimalShelterContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Animal> model = _db.Animals.ToList();
+      return View(model);
+    }
+
+    public ActionResult Create()
+    {
+        return View();
+    }
+
+// Add in additional info for animal coming from the form
+    [HttpPost]
+    public ActionResult Create(Animal animal)
+    {
+        _db.Animals.Add(animal);
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+    }
+
+// Add in additional info for animals
+    public ActionResult Details(int id)
+    {
+      Animal thisAnimal = _db.Animals.FirstOrDefault(animal => animal.AnimalId == id);
+      return View(thisAnimal);
+    }
+  }
+}
---
+++ AnimalShelter/Models/Animal.cs
@@ -0,0 +1,12 @@
+namespace AnimalShelter.Models
+{
+  public class Animal
+  {
+    public int AnimalId { get; set; }
+    public string Type { get; set; }
+    public string Name { get; set; }
+    public string Gender { get; set; }
+    public string Date_Admitted { get ; set; } // Date
+    public string Breed { get; set; }
+  }
+}
